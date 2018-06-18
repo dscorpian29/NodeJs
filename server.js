817a79786d4cbe5f72f1458b5808f0bec86b2bcd
hemant.n.sharma <hemant.n.sharma@accenture.com>
2018-06-18 11:14:24 +0530

    server.js updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world from a Node.js app!s is Updated. This is third update')
+    res.send('Hello world from a  Node.js app!s is Updated. This is third update')
 })
 var PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
